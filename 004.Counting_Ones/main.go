7e85233440abf282f9ecd81c1e790cf4e4a629da
Anderson <cadastros@andersonq.eti.br>
2017-10-07 20:32:23 -0300

    Fixed import

--- 004.Counting_Ones/main.go
+++ 004.Counting_Ones/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/AndersonQ/100-days-of-algorithms/003.Counting_Ones/count"
+	"github.com/AndersonQ/100-days-of-algorithms/004.Counting_Ones/count"
 )
 
 func main() {
